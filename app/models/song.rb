36bd1a90db2d58da84f6a3d33f83759e51fd53b8
Oz <ozzo3@yahoo.com>
2019-11-23 23:45:02 -0700

    initial push

---
+++ app/models/song.rb
@@ -0,0 +1,6 @@
+class Song < ApplicaionRecord
+  
+end
+
+#i think thats it but the file is actually called song.rb not sub
+#so this is my parent page right?
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :subs do
+    resources :topics
+  end
+  
+  resources :topics do
+    resources :comments
+  end
+    
+  # end
+
+
 end
