335c7be76feab3881157d67595ebaa021357049d
blackarchangel13 <57735715+blackarchangel13@users.noreply.github.com>
2019-11-29 23:42:28 -0700

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'songs/index'
+  get 'songs/create'
+  get 'artist/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :subs do
+    resources :topics
+  end
+  
+  resources :topics do
+    resources :comments
+  end
+    
+  # end
+
+
 end
